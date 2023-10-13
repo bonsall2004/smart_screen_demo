e8a338a04bf0512772e71e4d5026812c32f426f3
bonsall2004 <bonsall2004@gmail.com>
2023-10-13 07:30:56 +0100

    Boilerplate Template Code

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+
+#include <esp32_smartdisplay.h>
+#include <ui/ui.h>
+
+void millisecond_display_update()
+{
+    char time_buffer[32];
+    sprintf(time_buffer, "%d", millis());
+    lv_label_set_text(ui_lblMillisecondsValue, time_buffer);
+}
+
+void OnButtonClicked(lv_event_t *e)
+{
+    static uint8_t cnt = 0;
+    cnt++;
+    lv_label_set_text_fmt(ui_lblCountValue, "%d", cnt);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    smartdisplay_init();
+    ui_init();
+}
+
+void loop()
+{
+    lv_timer_handler();
+
+    auto r = (byte)(millis() / 75);
+    auto g = (byte)(millis() / 10);
+    auto b = (byte)(millis() / 150);
+    
+    smartdisplay_set_led_color(lv_color32_t({.ch = {.blue = 0, .green = 0, .red = 0}}));
+
+    millisecond_display_update();
+}
